2b472f570df73373ad174c296c5c07c92d61de5b
Dean Smith <Dean Smith>
2017-01-31 11:09:36 -0600

    The finished sandwith makergit status! Finallygit statusgit status!

--- sandwich-maker/javascripts/sandWichMaker.js
+++ sandwich-maker/javascripts/sandWichMaker.js
@@ -1,23 +1,23 @@
 "use strict";
  
-var sandwichMaker = (function(originalSandwich){
+var sandwichMaker = (function(){
   var totalPrice = 0;
    return {
     addTopping: function(toppingPrice) {
       totalPrice += toppingPrice;
-      console.log("The total price is now: " + totalPrice);
+      console.log("Your price: " + totalPrice);
     },
-    subtractTopping: function(event) {
-      totalPrice -= event;
-      console.log("The total price is now: " + totalPrice);
+    subtractTopping: function(toppingPrice) {
+      totalPrice -= toppingPrice;
+      console.log("Your price: " + totalPrice);
     },
-    finalPrice: function(end) {
+    finalPrice: function() {
       var end = totalPrice;
       return end;
-  }
+    }
 
 };
-   return originalSandwich;
+
 })(sandwichMaker || {});
 
 
